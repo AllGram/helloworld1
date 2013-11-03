b6a960bf101437a385f6877e3c2b8c24430b466d
Adalheidur Hreinsdottir <adalheidurh10@ru.is>
2013-11-03 20:04:41 +0000

    Add world class and test for it

--- src/main/java/is/ru/helloworld/HelloWorld.java
+++ src/main/java/is/ru/helloworld/HelloWorld.java
@@ -1,9 +1,10 @@
-package is.ru.helloworld;
+package is.ru.helloworld1;
  
  
  public class HelloWorld {
      public static void main(String[] args) {
-         System.out.println("Hello world!");
+     	World world = new World();
+        System.out.println(world.greet());
      }
  }
  
