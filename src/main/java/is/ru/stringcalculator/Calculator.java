5bb2c16d679ab8d6aceee0ee1a39d4f4e982583a
Adalheidur Hreinsdottir <adalheidurh10@ru.is>
2013-11-04 17:21:07 +0000

    Testing one number

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,5 +1,12 @@
 package is.ru.stringcalculator;
 
 public class Calculator {
-	
+	public static int add(String text){
+		if(text.equals("")){
+			return 0;
+		}
+		else{
+			return Integer.parseInt(text);
+		}
+	}
 }
